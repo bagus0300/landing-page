2524dd9a477cfd39732ccd96d7cf9e49e27e965d
sekai0330 <asamieto965@gmail.com>
2024-04-03 02:36:22 +0900

    home update

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,6 +1,5 @@
 export { default as HomeHero } from './hero'
 export { default as HomePopularCourse } from './mission'
-export { default as HomeFeature } from './feature'
 export { default as HomeTestimonial } from './services'
 export { default as HomeOurMentors } from './benifits'
 export { default as HomeNewsLetter } from './contact'
