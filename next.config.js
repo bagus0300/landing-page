7bbf7f1b6ca474b6e543dde6848d9872b2926071
sekai0330 <asamieto965@gmail.com>
2024-04-03 23:38:42 +0900

    update one issue

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  // more
 }
 
 module.exports = nextConfig
-
-
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,11 +9,11 @@ import { AppContextType, AppPropsType } from 'next/dist/shared/lib/utils'
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 
 interface DocumentProps {
-  emotionStylesTags: any[]
+  emotionStylesTags: React.ReactElement[]
 }
 
 class MyDocument extends Document<DocumentProps> {
-  render(): any {
+  render(): React.ReactElement {
     return (
       <Html lang="en">
         <Head>
