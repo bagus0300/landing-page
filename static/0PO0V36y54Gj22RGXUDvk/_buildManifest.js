47a05ca9fdb2c76d301d68fdfabc3cf2e802b56f
sekai0330 <asamieto965@gmail.com>
2024-04-03 20:28:55 +0900

    update build

---
+++ static/0PO0V36y54Gj22RGXUDvk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/77-b62f13135fc73bf5.js","static/chunks/pages/index-983e3008633be872.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
